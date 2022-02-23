65653e1932f5b593a60572b9265bd90f865fcc95
Arosennin <arek1arek4@onet.pl>
2022-02-23 10:30:00 +0100

    added very sussy something fin for now

--- amogos.py
+++ amogos.py
@@ -1,4 +1,24 @@
-import sys
+a = "amogus\nis sus"
+print(a)
 
-print(sys.version)
-print("sus amogus")
+b=3
+print(b)
+
+c=3.4
+d=2+3j
+print(c)
+print(d)
+e,f=3,5
+print(e+f)
+g=8
+print(b//g)
+print(g**b)
+g += 1
+print(g)
+
+
+a = 5
+b = 3
+c = a-b
+
+print("wynik dzialania %(z1)d - %(z2)d = %(z3)d" % {z1 : a,z2 : b,z3 : c})
