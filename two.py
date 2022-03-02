9a7bd37fe70721c7f22bc01ee9369f338c1a8ffc
Arosennin <arek1arek4@onet.pl>
2022-03-02 10:05:38 +0100

    hmmm

--- two.py
+++ two.py
@@ -42,14 +42,36 @@
 #     print(licznik)
 #     licznik += 1
 
-lista = [4, 6, 2, 5, 3, 9, 8, 7]
-a = input("podaj liczbe: ")
-licznik = 0
-while licznik != len(lista):
-    if int(a) - lista[licznik] == 0:
-        print("liczba " + str(a) + " - " + str(lista[licznik]) + " = 0")
-        break
-    else:
-        licznik += 1
-else:
-    print("żadna z wartosci")
+# lista = [4, 6, 2, 5, 3, 9, 8, 7]
+# a = input("podaj liczbe: ")
+# licznik = 0
+# while licznik != len(lista):
+#     if int(a) - lista[licznik] == 0:
+#         print("liczba " + str(a) + " - " + str(lista[licznik]) + " = 0")
+#         break
+#     else:
+#         licznik += 1
+# else:
+#     print("żadna z wartosci")
+
+# lista1 = [1,2,3,4,5,6,7,8]
+# lista2 = [9,10,1,2]
+# suma = []
+# for a in lista1:
+#     for b in lista2:
+#         wynik = a + b
+#         suma.append(wynik)
+#     print(suma)
+
+a = input("wczytaj liczbe 1: ")
+b = input("wczytaj liczbe 2: ")
+try:
+    a = int(a)
+    b = int(b)
+    wynik = a / b
+    print(wynik)
+except ZeroDivisionError:
+    print("sussy baka, nie dzieli sie przez 0")
+except ValueError:
+    print("sussy baka, nie wczytano liczby całkowitej")
+
