6eaa34620539425b04ed5c00c5241c75592ec21d
Arosennin <arek1arek4@onet.pl>
2022-03-02 09:45:41 +0100

    hmmm

--- two.py
+++ two.py
@@ -1,13 +1,55 @@
-a = 6
-b = 5
+# a = 6
+# b = 5
+#
+# if a > b:
+#     print("liczba a jest większa od b")
+# elif a < b:
+#     print("liczba b jest większa od a")
+# else:
+#     print("liczba a jest równa b")
 
-if a > b:
-    print("liczba a jest większa od b")
-elif a < b:
-    print("liczba b jest większa od a")
+# a = input("wczytaj liczbe: ")
+# print(a)
+# print(type(a))
+# a = int(a)
+# print(type(a))
+
+# a = input("wczytaj liczbe 1: ")
+# b = input("wczytaj liczbe 2: ")
+# c = input("wczytaj liczbe 3: ")
+# d = input("wczytaj liczbe 4: ")
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# d = int(d)
+#
+# if (a > b) & (c > d):
+#     print("liczba a jest większa od b i liczba c jest większa od d")
+# else:
+#     print("liczba a jest mniejsza od b lub liczba c jest mniejsza od d")
+
+# for a in range(0,7,1):
+#     print(a)
+
+# lista = ["a", 3, 4, 5.6]
+# for a in lista:
+#     print(a)
+# else:
+#     print("wyświetlone zostały elementy z listy")
+# licznik = 0
+
+# while licznik != 11:
+#     print(licznik)
+#     licznik += 1
+
+lista = [4, 6, 2, 5, 3, 9, 8, 7]
+a = input("podaj liczbe: ")
+licznik = 0
+while licznik != len(lista):
+    if int(a) - lista[licznik] == 0:
+        print("liczba " + str(a) + " - " + str(lista[licznik]) + " = 0")
+        break
+    else:
+        licznik += 1
 else:
-    print("liczba a jest równa b")
-
-a = input("wczytaj liczbe: ")
-print(a)
-print(type(a))
+    print("żadna z wartosci")
