bd62fbccf6d193b30ae02a7786aa3c41de7defc9
MMrok27 <97949402+MMrok27@users.noreply.github.com>
2022-06-11 22:10:47 +0200

    Create tablicatablic.c

---
+++ tablicatablic.c
@@ -0,0 +1,10 @@
+int **rezerwacja(int n,int m)
+{
+    int *tab=(int*)malloc(sizeof(int*)*n);
+    for(int i=0;i<n;i++)
+    {
+        (tab+i)=(int)malloc(sizeof(int)*m);
+    }
+    return tab;
+}
+tablica tablic
